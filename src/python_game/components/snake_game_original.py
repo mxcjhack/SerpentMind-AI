80bcd0d71459239ae062578bd01515d3cc926f80
mxcjhack <prasoongreat.mvp@gmail.com>
2023-12-23 17:55:10 +0530

    agent made

--- src/python_game/components/snake_game_original.py
+++ src/python_game/components/snake_game_original.py
@@ -2,7 +2,6 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
-import numpy as np
 
 pygame.init()
 font = pygame.font.SysFont('arial', 25)
@@ -15,15 +14,6 @@ BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0, 0, 0)
 
-"""
-reset
-reward
-play(action) -> direction
-game_itteration
-is_collision
-
-"""
-
 class Direction(Enum):
     RIGHT = 1
     LEFT = 2
@@ -32,7 +22,7 @@ class Direction(Enum):
 
 Point = namedtuple('Point', 'x , y')#light weight class
 
-class SankeGameAI:
+class SankeGame:
 
 
     def __init__(self, w = 640, h = 480):
@@ -43,9 +33,8 @@ class SankeGameAI:
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption("Sanke")
         self.clock = pygame.time.Clock()
-        self.reset()
 
-    def reset(self):
+        #init game state
         self.direction = Direction.RIGHT
 
         self.head = Point(self.w/2, self.h/2)
@@ -56,7 +45,6 @@ class SankeGameAI:
         self.score = 0
         self.food = None
         self._place_food()
-        self.frame_itteration = 0
     
     def _place_food(self):
         x = random.randint(0, (self.w-BLOK_SIZE)//BLOK_SIZE) * BLOK_SIZE
@@ -64,33 +52,36 @@ class SankeGameAI:
         self.food = Point(x, y)
         if self.food in self.snake:
             self._place_food()
-
-    def play_step(self, action):
-        self.frame_itteration += 1
+    def play_step(self):
         #1. Collect the user input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.direction = Direction.LEFT
+                elif event.key == pygame.K_RIGHT:
+                    self.direction = Direction.RIGHT
+                elif event.key == pygame.K_UP:
+                    self.direction = Direction.UP
+                elif event.key == pygame.K_DOWN:
+                    self.direction = Direction.DOWN
 
         #2. MOve
-        self._move(action)
+        self._move(self.direction)
         self.snake.insert(0, self.head)
 
         #3. Check if game over
-        reward = 0
         game_over = False
-        if self.is_collision() or self.frame_itteration > 100*len(self.snake):
+        if self._is_collision():
             game_over = True
-            reward = -10
-            return reward, game_over, self.score
+            return game_over, self.score
 
 
         #4. Place new food or just move
         if self.head == self.food:
             self.score += 1
-            reward = 10
             self._place_food()
         else:
             self.snake.pop()
@@ -101,7 +92,7 @@ class SankeGameAI:
 
         #6. Return game over and score
         game_over = False
-        return reward, game_over , self.score
+        return game_over , self.score
     
     def _update_ui(self):
         self.display.fill(BLACK)
@@ -116,42 +107,40 @@ class SankeGameAI:
         self.display.blit(text, [0, 0])
         pygame.display.flip()
     
-    def is_collision(self, pt = None):
-        if pt is None:
-            pt = self.head
-
-        if pt.x > self.w - BLOK_SIZE or pt.x < 0 or pt.y > self.h- BLOK_SIZE or pt.y < 0:
+    def _is_collision(self):
+        if self.head.x > self.w - BLOK_SIZE or self.head.x < 0 or self.head.y > self.h- BLOK_SIZE or self.head.y < 0:
             return True
-        if pt in self.snake[1:]:
+        if self.head in self.snake[1:]:
             return True
         return False
 
-    def _move(self, action):
-        #[straight, right, left]
-
-        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
-        idx = clock_wise.index(self.direction)
-
-        if np.array_equal(action, [1, 0, 0]):
-            new_dir = clock_wise[idx]#no change
-        elif np.array_equal(action, [0, 1, 0]):
-            next_idx = (idx +1) % 4
-            new_dir = clock_wise[next_idx]# right turn r ->d ->l ->u
-        else: #[0, 0, 1]
-            next_idx = (idx - 1) % 4
-            new_dir = clock_wise[next_idx]# left turn r ->u ->l ->d
-        
-        self.direction = new_dir
-
+    def _move(self, direction):
         x = self.head.x
         y = self.head.y
-        if self.direction == Direction.RIGHT:
+        if direction == Direction.RIGHT:
             x += BLOK_SIZE
-        elif self.direction == Direction.LEFT:
+        elif direction == Direction.LEFT:
             x -= BLOK_SIZE
-        elif self.direction == Direction.UP:
+        elif direction == Direction.UP:
             y -= BLOK_SIZE
-        elif self.direction == Direction.DOWN:
+        elif direction == Direction.DOWN:
             y += BLOK_SIZE
         
         self.head = Point(x , y)
+
+if __name__ == '__main__':
+    game = SankeGame()
+
+
+    #game loop
+    while True:
+        game_over, score = game.play_step()
+
+        if game_over:
+            break
+
+
+        #break if over
+    print("final score" , score)
+
+    pygame.quit()
